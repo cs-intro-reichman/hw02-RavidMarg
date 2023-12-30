ffd41b7a0903c7800db8d21673029492b4f2ccb9
RavidMarg <ravid.margalit@post.runi.ac.il>
2023-12-30 10:47:30 +0200

    Fixed prints

--- Reverse.java
+++ Reverse.java
@@ -6,11 +6,11 @@ public class Reverse {
 	public static void main (String[] args){
 		String str_to_reverse = (args[0]);
 		int str_length = str_to_reverse.length();
-		System.out.println(str_length);
 		String reversed_str = "";
 		for( int i = str_length - 1; 0 <= i; i-- ) {
 			reversed_str = reversed_str + str_to_reverse.charAt(i);
 		}
+		System.out.println(reversed_str);
 		System.out.println("The middle character is " + reversed_str.charAt(str_length / 2));
 	}
 }
